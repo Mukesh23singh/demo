417ef68814c596f838d846a0d80a2b4b30123361
Mukesh <mukesh23.singh@gmail.com>
2020-12-31 18:10:08 +0530

    adding mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,4 +114,14 @@ Rails.application.configure do
 
   # config/environments/production.rb
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  ActionMailer::Base.smtp_settings = {
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'yourapp.heroku.com',
+    authentication: :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
