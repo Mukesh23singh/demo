c8f7526546d5dc1184c6bcebec55dc7e2da493ad
Mukesh <mukesh23.singh@gmail.com>
2020-12-31 19:12:46 +0530

    adding custom domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -121,7 +121,7 @@ Rails.application.configure do
     address: ENV['MAILGUN_SMTP_SERVER'],
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: 'https://hidden-waters-10426.herokuapp.com',
+    domain: 'alayfinance.com',
     authentication: :plain
   }
   ActionMailer::Base.delivery_method = :smtp
