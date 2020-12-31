ed341c3fd706246f628bf9143425260606e6a5d0
Mukesh <mukesh23.singh@gmail.com>
2020-12-31 18:21:45 +0530

    adding domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,7 +120,7 @@ Rails.application.configure do
     address: ENV['MAILGUN_SMTP_SERVER'],
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: 'yourapp.heroku.com',
+    domain: 'https://hidden-waters-10426.herokuapp.com/',
     authentication: :plain
   }
   ActionMailer::Base.delivery_method = :smtp
